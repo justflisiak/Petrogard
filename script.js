8560d1f19208e0fb37f6a02825378803de17d592
justflisiak <44288233+justflisiak@users.noreply.github.com>
2019-02-04 14:45:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="styl.css" rel="stylesheet">
+
+    <title>Title</title>
+
+</head>
+
+<body>
+    <main>
+        <div class="logo">Petrogard resteurant</div>
+
+        <div class="gallery">gallery wanna be</div>
+
+        <div class="menu">
+
+            <div class="form">form<div>
+
+                    <input type="checkbox" name="vege" value="vege">vege
+
+                </div>
+                
+                          <div>
+                    <input type="checkbox" name="vegan" value="vegan">vegan
+                </div>
+                
+                <div>
+                    <input type="checkbox" name="noalcohol" value="noalcohol">no alcohol
+                </div>
+
+            </div>
+
+
+            <div class="courses">
+                course
+                <div class="starter">starter</div>
+                <div class="main">main</div>
+                <div class="dessert">dessert</div>
+                <div class="drinks">drinks</div>
+                <div class="sides">sides</div>
+            </div>
+
+
+        </div>
+
+    </main>
+
+    <footer> created by hdjkn</footer>
+
+</body>
+
+</html>
